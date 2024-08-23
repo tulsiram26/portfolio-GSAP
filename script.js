0c0267f3f659afc71cdfd7edb2877aff50d1cb37
tulsi <gavdetulsiram82@gmail.com>
2024-08-23 12:41:55 +0530

    into

---
+++ script.js
@@ -0,0 +1,25 @@
+var tl = gsap.timeline()
+
+gsap.from("#nav h3",{
+    y:-50,
+    duration:2,
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    stagger:0.3,
+})
+
+tl.from("#main h1",{
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4,
+})
+
+tl.from("img",{
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5,
+    stagger: 0.5,
+})
